6dd12eae9096fa31a29ebddefab46656129f9df5
Theory Ledbetter <trl3788@rit.edu>
2023-09-13 14:57:26 -0400

    Finished the Streaming Media Homework

---
+++ src/htmlResponses.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+// file system module
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+const page3 = fs.readFileSync(`${__dirname}/../client/client3.html`);
+
+const sendPage = (request, response, page) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(page);
+  response.end();
+}
+
+const getIndex = (request, response) => {
+  sendPage(request, response, index);
+};
+const getPage2 = (request, response) => {
+  sendPage(request, response, page2);
+}
+const getPage3 = (request, response) => {
+  sendPage(request, response, page3);
+}
+module.exports = {
+  getIndex,
+  getPage2,
+  getPage3,
+};
